015901827d63c293110af88e4c067ff6dffe2719
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-04-16 12:01:25 -0300

    Adicionar arquivos de projeto.

---
+++ APIDevSteam/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿
+// Arquivo: Usuario.cs
+
+using Microsoft.AspNetCore.Identity;
+
+namespace APIDevSteam.Models
+{
+    public class Usuario : IdentityUser
+
+    {
+        public Usuario() : base()
+        { }
+        public string? NomeCompleto { get; set; }
+        public DateOnly DataNascimento { get; set; }
+
+
+    }
+}
+
